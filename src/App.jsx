75c0588e3b5cd300359a1752188d0eb566bb7eb3
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-02-19 19:42:38 -0300

    Suporte a theme.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Stack, IconButton, Box, CssBaseline, Button } from "@mui/material";
+import { Stack, IconButton, Box, CssBaseline, Button, ThemeProvider } from "@mui/material";
 
 import { useState } from "react";
 
 import ResetScore from "./ResetScore";
 import Rules from "./Rules";
 import Header from "./Header"
+import { lightTheme } from "./theme/theme";
 
 let playerScore = localStorage.getItem("playerScore");
 let computerScore = localStorage.getItem("computerScore")
@@ -76,77 +77,79 @@ function App() {
 
   return (
     <>
-      <CssBaseline />
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
 
-      {/* Container principal que engolba tudo*/}
-      <Stack height={"100vh"} sx={{
-        background: "radial-gradient(at top, hsl(214, 47%, 23%), hsl(237, 48%, 15%))"
-      }}>
+        {/* Container principal que engolba tudo*/}
+        <Stack height={"100vh"} sx={{
+          background: "radial-gradient(at top, hsl(214, 47%, 23%), hsl(237, 48%, 15%))"
+        }}>
 
-        <Stack py={5} gap={10} width={"100%"} alignItems={"center"} >
+          <Stack py={5} gap={10} width={"100%"} alignItems={"center"} >
 
-          <Header />
+            <Header />
 
-          <Stack justifyContent={"center"} alignItems={"center"} width={"80%"} >
+            <Stack justifyContent={"center"} alignItems={"center"} width={"80%"} >
 
-            {/* Box que armazena icones de pedra papel e tesoura e o triangulo*/}
-            <Box gap={20} width={"75%"} height="300px" sx={{
-              background: "url(/bg-triangle.svg) no-repeat center", display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}>
+              {/* Box que armazena icones de pedra papel e tesoura e o triangulo*/}
+              <Box gap={20} width={"75%"} height="300px" sx={{
+                background: "url(/bg-triangle.svg) no-repeat center", display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}>
 
-              <Box width={"100%"} sx={{
-                display: "flex", alignItems: 'center', justifyContent: "center",
-                position: "relative", top: "-40px"
-              }} gap={20}>
+                <Box width={"100%"} sx={{
+                  display: "flex", alignItems: 'center', justifyContent: "center",
+                  position: "relative", top: "-40px"
+                }} gap={20}>
 
-                {/* Box que armeza o botão de papel*/}
-                <Box borderRadius={"50%"} height={"170px"} width={"170px"} sx={{
-                  display: "flex",
-                  justifyContent: "center", alignItems: "center", background: "hsl(230, 89%, 62%)",
-                  borderBottom: "10px solid hsl(229, 64%, 46%)"
-                }}>
+                  {/* Box que armeza o botão de papel*/}
+                  <Box borderRadius={"50%"} height={"170px"} width={"170px"} sx={{
+                    display: "flex",
+                    justifyContent: "center", alignItems: "center", background: "hsl(230, 89%, 62%)",
+                    borderBottom: "10px solid hsl(229, 64%, 46%)"
+                  }}>
 
-                  <Button variant="contained" sx={{ height: "130px", width: "130px", boxShadow: "none", background: "white", borderRadius: "50%", borderTop: "10px solid grey", ":hover": { boxShadow: "none" } }}>
-                    <img src="/icon-paper.svg" style={{ height: "60%", width: "60%" }} />
-                  </Button>
-                </Box>
+                    <Button variant="contained" sx={{ height: "130px", width: "130px", boxShadow: "none", background: "white", borderRadius: "50%", borderTop: "10px solid grey", ":hover": { boxShadow: "none" } }}>
+                      <img src="/icon-paper.svg" style={{ height: "60%", width: "60%" }} />
+                    </Button>
+                  </Box>
 
-                {/* Box que armazena botão de tesoura*/}
-                <Box borderRadius={"50%"} height={"170px"} width={"170px"} sx={{
-                  display: "flex",
-                  justifyContent: "center", alignItems: "center", background: "hsl(39, 89%, 49%)",
-                  borderBottom: "10px solid hsl(28, 76%, 44%)"
-                }}>
-                  <Button variant="contained" sx={{ height: "130px", width: "130px", boxShadow: "none", background: "white", borderRadius: "50%", borderTop: "10px solid grey", ":hover": { boxShadow: "none" } }}>
-                    <img src="/icon-scissors.svg" style={{ height: "70%", width: "70%" }} />
-                  </Button>
-                </Box>
+                  {/* Box que armazena botão de tesoura*/}
+                  <Box borderRadius={"50%"} height={"170px"} width={"170px"} sx={{
+                    display: "flex",
+                    justifyContent: "center", alignItems: "center", background: "hsl(39, 89%, 49%)",
+                    borderBottom: "10px solid hsl(28, 76%, 44%)"
+                  }}>
+                    <Button variant="contained" sx={{ height: "130px", width: "130px", boxShadow: "none", background: "white", borderRadius: "50%", borderTop: "10px solid grey", ":hover": { boxShadow: "none" } }}>
+                      <img src="/icon-scissors.svg" style={{ height: "70%", width: "70%" }} />
+                    </Button>
+                  </Box>
 
-              </Box  >
+                </Box  >
 
-              <Box sx={{ position: "relative", top: "-110px" }}>
-                <Box borderRadius={"50%"} height={"170px"} width={"190px"} sx={{
-                  display: "flex",
-                  justifyContent: "center", alignItems: "center", background: "hsl(349, 71%, 52%)",
-                  borderBottom: "10px solid hsl(347, 75%, 35%)",
-                }}>
-                  <Button variant="contained" sx={{ height: "130px", width: "130px", boxShadow: "none", background: "white", borderRadius: "50%", borderTop: "10px solid grey", ":hover": { boxShadow: "none" } }}>
-                    <img src="/icon-rock.svg" style={{ height: "60%", width: "60%" }} />
-                  </Button>
+                <Box sx={{ position: "relative", top: "-110px" }}>
+                  <Box borderRadius={"50%"} height={"170px"} width={"190px"} sx={{
+                    display: "flex",
+                    justifyContent: "center", alignItems: "center", background: "hsl(349, 71%, 52%)",
+                    borderBottom: "10px solid hsl(347, 75%, 35%)",
+                  }}>
+                    <Button variant="contained" sx={{ height: "130px", width: "130px", boxShadow: "none", background: "white", borderRadius: "50%", borderTop: "10px solid grey", ":hover": { boxShadow: "none" } }}>
+                      <img src="/icon-rock.svg" style={{ height: "60%", width: "60%" }} />
+                    </Button>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
 
+            </Stack>
+          </Stack>
+
+          <Stack direction={"row-reverse"} spacing={3} px={5} alignItems={"end"}>
+            <ResetScore />
+            <Rules />
           </Stack>
         </Stack>
-
-        <Stack direction={"row-reverse"} spacing={3} px={5} alignItems={"end"}>
-          <ResetScore />
-          <Rules />
-        </Stack>
-      </Stack>
+      </ThemeProvider>
 
     </>
   )
